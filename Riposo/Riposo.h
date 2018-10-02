545c9fe576e2db6ebf39e452a65122a0026c6769
Anthony Colangelo <me@acolangelo.com>
2018-10-02 10:34:28 -0600

    Initial Commit

---
+++ Riposo/Riposo.h
@@ -0,0 +1,19 @@
+//
+//  Riposo.h
+//  Riposo
+//
+//  Created by Anthony Colangelo on 10/2/18.
+//  Copyright © 2018 Dot First. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Riposo.
+FOUNDATION_EXPORT double RiposoVersionNumber;
+
+//! Project version string for Riposo.
+FOUNDATION_EXPORT const unsigned char RiposoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Riposo/PublicHeader.h>
+
+
