49b2e8ae0f8e92bfd57fc21a63dd4ae401820188
Anthony Colangelo <me@acolangelo.com>
2019-01-24 10:43:51 -0500

    Improved JSON parsing and request body handling.

--- Riposo/Server.swift
+++ Riposo/Server.swift
@@ -33,29 +33,30 @@ public class Server {
         self.defaultHeaders = headers
     }
     
-    public func get(_ path: String? = nil, parameters: [String: Any]? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        Server.get(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
+    public func get(_ path: String? = nil, parameters: [String: Any]? = nil, body: Data? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        Server.get(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), body: body, headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
     }
     
-    public func post(_ path: String? = nil, parameters: [String: Any]? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        Server.post(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
+    public func post(_ path: String? = nil, parameters: [String: Any]? = nil, body: Data? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        Server.post(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), body: body, headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
     }
     
-    public func patch(_ path: String? = nil, parameters: [String: Any]? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        Server.patch(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
+    public func patch(_ path: String? = nil, parameters: [String: Any]? = nil, body: Data? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        Server.patch(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), body: body, headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
     }
     
-    public func put(_ path: String? = nil, parameters: [String: Any]? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        Server.put(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
+    public func put(_ path: String? = nil, parameters: [String: Any]? = nil, body: Data? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        Server.put(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), body: body, headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
     }
     
-    public func delete(_ path: String? = nil, parameters: [String: Any]? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        Server.delete(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
+    public func delete(_ path: String? = nil, parameters: [String: Any]? = nil, body: Data? = nil, headers: [String: Any]? = nil, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        Server.delete(url: url.appendingPathComponent(path ?? ""), parameters: defaultParameters.merging(parameters ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), body: body, headers: defaultHeaders.merging(headers ?? [String: Any](), uniquingKeysWith:  { (_, new) in new }), completionHandler: completionHandler)
     }
     
     //MARK: - Utility Methods
-    public class func request(method: HTTPMethod, url: URL, parameters: [String: Any]?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+    public class func request(method: HTTPMethod, url: URL, parameters: [String: Any]?, body: Data?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
         var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
         
         if let parameters = parameters {
             if (method == .get) {
@@ -69,6 +70,10 @@ public class Server {
             }
         }
         
+        if let body = body {
+            request.httpBody = body
+        }
+        
         if let headers = headers {
             for (headerKey, headerValue) in headers {
                 request.addValue(headerValue as! String, forHTTPHeaderField: headerKey)
@@ -78,41 +83,41 @@ public class Server {
         Server.session.dataTask(with: request) { data, response, error in
             guard let httpResponse = response as? HTTPURLResponse else {
                 DispatchQueue.main.async {
-                    completionHandler(Response(code: 0, json: nil, headers: nil, error: error))
+                    completionHandler(Response(code: 0, data: data, json: nil, headers: nil, error: error, request: request))
                 }
                 return
             }
-            var json: [String: Any]?
-            if let data = data {
-                do {
-                    json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
-                } catch {
-                    debugPrint("JSON read error: \(error)")
+            var json: Any?
+            if let data = data, let parsedJSON = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions()) {
+                if let jsonObject = parsedJSON as? [String: Any] {
+                    json = jsonObject
+                } else if let jsonArray = parsedJSON as? [Any] {
+                    json = jsonArray
                 }
             }
             DispatchQueue.main.async {
-                completionHandler(Response(code: httpResponse.statusCode, json: json, headers: httpResponse.allHeaderFields as? [String : Any], error: error))
+                completionHandler(Response(code: httpResponse.statusCode, data: data, json: json, headers: httpResponse.allHeaderFields as? [String : Any], error: error, request: request))
             }
-        }.resume()
+            }.resume()
     }
     
-    public class func get(url: URL, parameters: [String: Any]?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        request(method: .get, url: url, parameters: parameters, headers: headers, completionHandler: completionHandler)
+    public class func get(url: URL, parameters: [String: Any]?, body: Data?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        request(method: .get, url: url, parameters: parameters, body: body, headers: headers, completionHandler: completionHandler)
     }
     
-    public class func post(url: URL, parameters: [String: Any]?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        request(method: .post, url: url, parameters: parameters, headers: headers, completionHandler: completionHandler)
+    public class func post(url: URL, parameters: [String: Any]?, body: Data?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        request(method: .post, url: url, parameters: parameters, body: body, headers: headers, completionHandler: completionHandler)
     }
     
-    public class func patch(url: URL, parameters: [String: Any]?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        request(method: .patch, url: url, parameters: parameters, headers: headers, completionHandler: completionHandler)
+    public class func patch(url: URL, parameters: [String: Any]?, body: Data?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        request(method: .patch, url: url, parameters: parameters, body: body, headers: headers, completionHandler: completionHandler)
     }
     
-    public class func put(url: URL, parameters: [String: Any]?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        request(method: .put, url: url, parameters: parameters, headers: headers, completionHandler: completionHandler)
+    public class func put(url: URL, parameters: [String: Any]?, body: Data?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        request(method: .put, url: url, parameters: parameters, body: body, headers: headers, completionHandler: completionHandler)
     }
     
-    public class func delete(url: URL, parameters: [String: Any]?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
-        request(method: .delete, url: url, parameters: parameters, headers: headers, completionHandler: completionHandler)
+    public class func delete(url: URL, parameters: [String: Any]?, body: Data?, headers: [String: Any]?, completionHandler: @escaping (Response?) -> () = {(_: Response?) -> () in }) {
+        request(method: .delete, url: url, parameters: parameters, body: body, headers: headers, completionHandler: completionHandler)
     }
 }
