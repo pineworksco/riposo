38b9f8290b5fa73ad7eac769ab631fcd36dc1c66
Anthony Colangelo <me@acolangelo.com>
2019-01-24 10:43:10 -0500

    Added raw data and original request to Response.

--- Riposo/Response.swift
+++ Riposo/Response.swift
@@ -11,22 +11,32 @@ public struct Response {
     
     //MARK: Properties
     public private(set) var status: Status,
-                            json: [String: Any]?,
+                            data: Data?,
+                            json: Any?,
                             headers: [String: Any]?,
-                            error: Error?
+                            error: Error?,
+                            request: URLRequest?
     
     //MARK: Initializers
-    public init(code: Int, json: [String: Any]?, error: Error?) {
+    public init(code: Int, data: Data?, error: Error?) {
+        self.status = Status(code: code)
+        self.data = data
+        self.error = error
+    }
+    
+    public init(code: Int, json: Any?, error: Error?) {
         self.status = Status(code: code)
         self.json = json
         self.error = error
     }
     
-    public init(code: Int, json: [String: Any]?, headers: [String: Any]?, error: Error?) {
+    public init(code: Int, data: Data?, json: Any?, headers: [String: Any]?, error: Error?, request: URLRequest?) {
         self.status = Status(code: code)
+        self.data = data
         self.json = json
         self.headers = headers
         self.error = error
+        self.request = request
     }
     
 }
